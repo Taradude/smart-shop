86eef9da77d792865655b723cd9997a2857b530c
Taradude <131863917+Taradude@users.noreply.github.com>
2023-11-10 19:17:24 +0200

    Header (#2)
    * Header
    
    * fix

---
+++ src/components/TheHeader.vue
@@ -0,0 +1,22 @@
+<template>
+  <header class="header">
+    <nav>
+      <router-link to=""> Home Page</router-link>
+      <router-view />
+    </nav>
+  </header>
+</template>
+
+<script lang="ts">
+import { Vue, Component } from 'vue-property-decorator'
+
+@Component
+export default class TheHeader extends Vue {}
+</script>
+
+<style lang="scss">
+.header {
+  padding: 32px 48px;
+  background-color: $blue;
+}
+</style>
---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="main-layout">
+    <TheHeader />
+    <div class="main-layout__page">
+      <router-view />
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator'
+import TheHeader from '@/components/TheHeader.vue'
+
+@Component({
+  components: {
+    TheHeader,
+  },
+})
+export default class MainLayout extends Vue {}
+</script>
+
+<style lang="scss">
+.main-layout {
+  &__page {
+    padding: 64px 48px;
+  }
+}
+</style>

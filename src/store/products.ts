bfca8e8bd6f95dae9386dbaefb8c0d80d10b5695
Taradude <taraha2012@gmail.com>
2023-12-12 19:21:43 +0200

    fix

--- src/store/products.ts
+++ src/store/products.ts
@@ -9,7 +9,8 @@ export default {
     currentProduct: {},
     currentPage: 1,
     productsAmount: 0,
-    productsLimit: 8,
+    productsLimit: 7,
+    pagesAmount: 0,
   },
   mutations: {
     setProducts(state: any, newProductsList: IProduct[]) {
@@ -27,14 +28,22 @@ export default {
     decreaseCurrentPage(state: any) {
       state.currentPage--
     },
+    setCurrentPage(state: any, page: string) {
+      state.currentPage = page
+    },
+    setPagesAmount(state: any, pages: number) {
+      state.pagesAmount = pages
+    },
   },
   actions: {
     async getProducts({ commit, state }: any) {
       const { data } = await instanceApi.get(
-        `products?limit=${state.productsLimit}&skip=${state.currentPage * state.productsLimit}`
+        `products?limit=${state.productsLimit}&skip=${(state.currentPage - 1) * state.productsLimit}`
       )
+      const pagesAmount = Math.ceil(data.total / state.productsLimit)
       commit('setProducts', data.products)
       commit('setProductsAmount', data.total)
+      commit('setPagesAmount', pagesAmount)
     },
     nextPage({ commit, dispatch }: any) {
       commit('increaseCurrentPage')
